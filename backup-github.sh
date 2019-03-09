f4036ae5744629cb82436f6ba6856c6b62ddac45
rb_de0 <rebirth.de0@gmail.com>
2019-03-10 02:23:14 +0900

    Fix output

--- backup-github.sh
+++ backup-github.sh
@@ -7,18 +7,18 @@ dir_name=Github_Backup_$(date +%Y%m%d_%H)
 mkdir $dir_name
 cd $dir_name
 
-json=$(curl -H "Authorization: token $access_token" https://api.github.com/user/repos)
+json=$(curl -Ss -H "Authorization: token $access_token" https://api.github.com/user/repos)
 len=$(echo $json | jq length)
 
 for i in $( seq 0 $(($len - 1)) ); do
   ssh_url=$(echo $json | jq -r .[$i].ssh_url)
   name=$(echo $json | jq -r .[$i].name)
 
-  git clone $ssh_url
+  git clone -q $ssh_url
   cd $name
 
   for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
-    git branch --track ${branch#remotes/origin/} $branch
+    git branch --track ${branch#remotes/origin/} $branch 2>&1
   done
 
   cd ..
